2e60d179717b6ef12e57cf8487a9f4d3123e7ef0
Csanád Kölcsei <csanad.kolcsei@nebu.local>
2020-10-30 12:28:30 +0100

    Finished section 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,19 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'comma-dangle': [
+      'error',
+      {
+          arrays: 'never',
+          objects: 'never',
+          imports: 'never',
+          exports: 'never',
+          functions: 'never'
+      }
+  ],
+  'arrow-body-style': [
+      'error',
+      'as-needed'
+  ]
   },
 };
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@nestjs/common'
+import { Task, TaskStatus } from './task.model'
+import { v1 as uuid } from 'uuid'
+import { CreateTaskDto } from './dto/create-task.dto'
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto'
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = []
+
+    getAllTasks(): Task[] {
+        return this.tasks
+    }
+
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { status, searchTerm } = filterDto
+        let tasks = this.getAllTasks()
+
+        if (status) {
+            tasks = tasks.filter(task => task.status === status)
+        }
+
+        if (searchTerm) {
+            tasks = tasks.filter(
+                task =>
+                    task.title.includes(searchTerm) ||
+                    task.description.includes(searchTerm)
+            )
+        }
+
+        return tasks
+    }
+
+    getTaskById(id: string): Task {
+        return this.tasks.find(task => task.id === id)
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const { title, description } = createTaskDto
+
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        }
+
+        this.tasks.push(task)
+        return task
+    }
+
+    updateTask(id: string, property: string, newValue: string) {
+        const task = this.getTaskById(id)
+        task[property] = newValue
+        return task
+    }
+
+    deleteTask(id: string): void {
+        this.tasks = this.tasks.filter(task => id !== task.id)
+    }
+}
