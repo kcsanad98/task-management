3dcf7c70028d20cef255c7b18e0b0a526da85c64
Csanád Kölcsei <csanad.kolcsei@nebu.local>
2020-10-30 14:13:04 +0100

    Set up connection with pg db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { TasksModule } from './tasks/tasks.module'
+import { typeormConfig } from './config/typeorm.config'
 
 @Module({
-    imports: [TasksModule]
+    imports: [
+        TasksModule,
+        TypeOrmModule.forRoot(typeormConfig)
+    ]
 })
 export class AppModule {}
